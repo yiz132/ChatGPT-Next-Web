503a1e103c64fb39e01f2112b0fb37d7b168e45f
James Zhang <yiz132@pitt.edu>
2023-10-11 00:17:05 +0800

    encrypt with time

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -2,6 +2,7 @@ import { NextRequest } from "next/server";
 import { getServerSideConfig } from "../config/server";
 import md5 from "spark-md5";
 import { ACCESS_CODE_PREFIX } from "../constant";
+import * as CryptoJS from "crypto-js";
 
 function getIP(req: NextRequest) {
   let ip = req.ip ?? req.headers.get("x-real-ip");
@@ -24,11 +25,47 @@ function parseApiKey(bearToken: string) {
   };
 }
 
+const FIXED_SYMMETRIC_KEY = CryptoJS.enc.Utf8.parse("some_secure_key_32bytes!");
+const IV = CryptoJS.enc.Utf8.parse("16_byte_iv_here!");
+
+function decryptPublicKey(encryptedKey: string): string {
+  // Decode the base64 encrypted data
+  const decodedEncryptedData = CryptoJS.enc.Base64.parse(encryptedKey);
+
+  // Splitting encrypted data and date
+  const encryptedData = decodedEncryptedData.words.slice(
+    0,
+    decodedEncryptedData.sigBytes / 4 - 2,
+  ); // remove date part
+  const encryptedDataWordArray = CryptoJS.lib.WordArray.create(encryptedData);
+
+  // Decrypt
+  const decryptedData = CryptoJS.AES.decrypt(
+    { ciphertext: encryptedDataWordArray } as any,
+    FIXED_SYMMETRIC_KEY,
+    {
+      iv: IV,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    },
+  );
+
+  // Convert to string
+  return decryptedData.toString(CryptoJS.enc.Utf8);
+}
+
 export function auth(req: NextRequest) {
   const authToken = req.headers.get("Authorization") ?? "";
 
   // check if it is openai api key or user token
-  const { accessCode, apiKey: token } = parseApiKey(authToken);
+  let { accessCode, apiKey: token } = parseApiKey(authToken);
+
+  console.log("[Auth] got access code:", accessCode);
+
+  if (accessCode.length !== 16) {
+    accessCode = decryptPublicKey(accessCode);
+    console.log("[Auth] decrypted access code:", accessCode);
+  }
 
   const hashedCode = md5.hash(accessCode ?? "").trim();
 
